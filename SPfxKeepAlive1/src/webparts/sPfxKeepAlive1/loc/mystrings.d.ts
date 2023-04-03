33ded2225412525bc620552d64acd4f5b7ecf6c4
Darrell Johnson <djtrustgod@gmail.com>
2023-04-03 11:24:20 -0700

    commit

---
+++ SPfxKeepAlive1/src/webparts/sPfxKeepAlive1/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISPfxKeepAlive1WebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'SPfxKeepAlive1WebPartStrings' {
+  const strings: ISPfxKeepAlive1WebPartStrings;
+  export = strings;
+}

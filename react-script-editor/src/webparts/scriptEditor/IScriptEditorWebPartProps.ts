8430d50a92cb1fcdd051f898d5669f42c9becc89
Darrell Johnson <djtrustgod@gmail.com>
2023-06-23 00:01:33 -0700

    Big old delete

--- react-script-editor/src/webparts/scriptEditor/IScriptEditorWebPartProps.ts
+++
@@ -1,9 +0,0 @@
-export interface IScriptEditorWebPartProps {
-  script: string;
-  useExternalScript: boolean;
-  externalScript?: string;
-  title: string;
-  removePadding: boolean;
-  spPageContextInfo: boolean;
-  teamsContext: boolean;
-}
